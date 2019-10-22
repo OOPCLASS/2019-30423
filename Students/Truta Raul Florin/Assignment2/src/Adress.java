f2a42197a10d6175fb9308817b83c85447b6f044
truflorau <raultruta25@gmail.com>
2019-10-22 08:57:18 +0300

    Uploaded the second assignment (building)

---
+++ Students/Truta Raul Florin/Assignment2/src/Adress.java
@@ -0,0 +1,14 @@
+
+public class Adress {
+	
+	public String street;
+	public Integer number;
+	public String city;
+	
+	public void printAdress() {
+		
+		System.out.print(street + ", ");
+		System.out.print(number + ", ");
+		System.out.println(city);
+	}
+}
