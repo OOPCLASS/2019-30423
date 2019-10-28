6e4add0699eb483a4b9bda85f80d4a8c6500a79c
Valeriu Sabau <valeriusabau29@gmail.com>
2019-10-28 19:59:26 +0200

    Assigment 2

---
+++ Students/Sabau Valeriu Ilie/assigment2/Unit Test/bank/SavingAccountTest.java
@@ -0,0 +1,37 @@
+package bank;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SavingAccountTest {
+    private final double epsilon = 0.00001;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addInterestToBalancePositiveTest() {
+        double sum = 10;
+        SavingAccount account = new SavingAccount(0);
+        account.deposit(sum);
+        Assertions.assertEquals(account.getBalance(), sum, epsilon);
+        account.addInterestToBalance();
+        Assertions.assertEquals(account.getBalance(), sum + sum * account.getInterest(), epsilon);
+    }
+
+    @Test
+    void addInterestToBalanceNegativeTest() {
+        SavingAccount account = new SavingAccount(0);
+        account.withdraw(10);
+        Assertions.assertEquals(account.getBalance(), -10.0, epsilon);
+    }
+}
---
+++ Students/Sabau Valeriu Ilie/assigment2/src/bank/Bank.java
@@ -0,0 +1,57 @@
+package bank;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.lang.Object;
+
+public class Bank {
+
+    private Account[] accounts;
+    private int numberOfAccounts = 0;
+    private final int sizeOfAccounts = 200;
+
+    public Bank() {
+        accounts = new Account[sizeOfAccounts];
+    }
+
+    public void openAccount(Account newAccount) {
+        if (sizeOfAccounts > numberOfAccounts)
+            accounts[numberOfAccounts++] = newAccount;
+    }
+
+    public void closeAccount(int index) {
+        if (numberOfAccounts - 1 - index >= 0)
+            System.arraycopy(accounts, index + 1, accounts, index, numberOfAccounts - 1 - index);
+        numberOfAccounts--;
+    }
+
+    public void updateAccounts() {
+        for (int i = 0; i < numberOfAccounts; i++) {
+            if (accounts[i] instanceof SavingAccount) {
+                SavingAccount savingAccount = (SavingAccount) accounts[i];
+                savingAccount.addInterestToBalance();
+            }
+            if (accounts[i] instanceof CurrentAccount) {
+                CurrentAccount currentAccount = (CurrentAccount) accounts[i];
+                if (currentAccount.isOverdrawn()) {
+                    System.out.println("send message to currentAccount " + currentAccount.getAccountNumber());
+                }
+            }
+        }
+    }
+
+    public void payDividends(int accountNumber, double sum) {
+        for (Account account : accounts) {
+
+            if (account != null && account.getAccountNumber() == accountNumber) {
+                account.deposit(sum);
+            }
+        }
+    }
+
+    public void printAllAccounts() {
+        for (int i = 0; i < numberOfAccounts; i++) {
+            System.out.println(accounts[i].toString());
+        }
+    }
+}
