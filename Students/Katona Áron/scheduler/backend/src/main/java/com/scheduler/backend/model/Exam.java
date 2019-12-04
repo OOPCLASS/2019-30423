f8189ee557ef24e669564e3519835bf55ae77f39
Katona Áron <katonaaron01@gmail.com>
2019-12-04 20:05:24 +0200

    scheduler/backend: exam getters and setters

--- Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Exam.java
+++ Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Exam.java
@@ -6,4 +6,20 @@ public class Exam extends Entity {
 
 	private Date from;
 	private Date to;
+
+	public Date getFrom() {
+		return from;
+	}
+
+	public void setFrom(Date from) {
+		this.from = from;
+	}
+
+	public Date getTo() {
+		return to;
+	}
+
+	public void setTo(Date to) {
+		this.to = to;
+	}
 }
