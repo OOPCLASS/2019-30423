32639caf189ca96e946b73ac4d1185a3e7e175a4
Katona Áron <katonaaron01@gmail.com>
2019-12-06 14:50:52 +0200

    Update Entity.java
    equals and hashcode

--- Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Entity.java
+++ Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Entity.java
@@ -1,6 +1,7 @@
 package com.scheduler.backend.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public abstract class Entity implements Serializable {
 
@@ -12,5 +13,18 @@ public abstract class Entity implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Entity)) return false;
+		Entity entity = (Entity) o;
+		return Objects.equals(getId(), entity.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
 	}
 }
