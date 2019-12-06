7f08fb74a6cfb68930688e080fdb8eca91c96ef7
Katona Áron <katonaaron01@gmail.com>
2019-12-06 14:18:54 +0200

    PersonService implemented

--- Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/service/PersonService.java
+++ Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/service/PersonService.java
@@ -1,38 +1,35 @@
 package com.scheduler.backend.service;
 
-import java.util.List;
-
 import com.scheduler.backend.model.Person;
 import com.scheduler.backend.repository.PersonRepository;
 
+import java.util.List;
+
 public class PersonService implements Service<Person> {
 
-	private PersonRepository personRepository;
+    private PersonRepository personRepository;
 
-	public PersonService() {
-		this.personRepository = new PersonRepository();
-	}
+    public PersonService() {
+        this.personRepository = new PersonRepository();
+    }
 
-	@Override
-	public Person save(Person entity) {
-		return personRepository.save(entity);
-	}
+    @Override
+    public Person save(Person entity) {
+        return personRepository.save(entity);
+    }
 
-	@Override
-	public Person findById(Long id) {
-		// call findById on repository
-		return null;
-	}
+    @Override
+    public Person findById(Long id) {
+        return personRepository.findById(id);
+    }
 
-	@Override
-	public List<Person> findAll() {
-		// call findAll on repository
-		return personRepository.findAll();
-	}
+    @Override
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
 
-	@Override
-	public boolean delete(Person entity) {
-		// call delete on repository
-		return false;
-	}
+    @Override
+    public boolean delete(Person entity) {
+        return personRepository.delete(entity);
+    }
 }
