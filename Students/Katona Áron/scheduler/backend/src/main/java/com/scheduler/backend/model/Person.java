25f33001c2328be760ad80a0188286b623dcd5f4
Katona Áron <katonaaron01@gmail.com>
2019-12-06 11:32:42 +0200

    Person.java: Added constructors, equal and hashcode.

--- Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Person.java
+++ Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Person.java
@@ -1,12 +1,30 @@
 package com.scheduler.backend.model;
 
+import java.util.Objects;
+
 public class Person extends Entity {
 
 	private String firstName;
 	private String lastName;
 	private PersonType type;
 	private Long calendarId;
-	
+
+	public Person() {
+	}
+
+	public Person(String firstName, String lastName, PersonType type) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.type = type;
+	}
+
+	public Person(String firstName, String lastName, PersonType type, Long calendarId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.type = type;
+		this.calendarId = calendarId;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -30,5 +48,21 @@ public class Person extends Entity {
 	}
 	public void setCalendarId(Long calendarId) {
 		this.calendarId = calendarId;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Person)) return false;
+		Person person = (Person) o;
+		return getFirstName().equals(person.getFirstName()) &&
+				getLastName().equals(person.getLastName()) &&
+				getType() == person.getType() &&
+				Objects.equals(getCalendarId(), person.getCalendarId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getFirstName(), getLastName(), getType(), getCalendarId());
 	}
 }
