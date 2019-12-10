fbb42e8a38fa5f23888ec34fdcff82d27b5023d6
Katona Áron <katonaaron01@gmail.com>
2019-12-10 17:39:23 +0200

    equals and hashcode + constructor

--- Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Exam.java
+++ Students/Katona Áron/scheduler/backend/src/main/java/com/scheduler/backend/model/Exam.java
@@ -1,11 +1,20 @@
 package com.scheduler.backend.model;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Exam extends Entity {
 
 	private Date from;
 	private Date to;
+
+	public Exam() {
+	}
+
+	public Exam(Date from, Date to) {
+		this.from = from;
+		this.to = to;
+	}
 
 	public Date getFrom() {
 		return from;
@@ -21,5 +30,19 @@ public class Exam extends Entity {
 
 	public void setTo(Date to) {
 		this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Exam)) return false;
+		Exam exam = (Exam) o;
+		return Objects.equals(getFrom(), exam.getFrom()) &&
+				Objects.equals(getTo(), exam.getTo());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getFrom(), getTo());
 	}
 }
